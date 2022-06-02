9cb9e531d4dd2dcb13e5325b9eb8caad086844e5
Taehoon Kim <hyri0510@gmail.com>
2022-06-02 23:02:49 +0900

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '../views/HomePage.vue'
+import Robot from '../views/Robot.vue'
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes: [
+		{
+			path: '/',
+			component: HomePage
+		},
+		{
+			path: '/about',
+			component: () => import('../views/About.vue')
+		},
+		{
+			path: '/robot',
+			component: Robot
+		},
+	],
+})
+
+export default router
---
+++ src/views/Robot.vue
@@ -0,0 +1,6 @@
+<template>
+  <main class="robot-page">
+      <h1>Robot</h1>
+      <p>This is the robot page</p>
+  </main>
+</template>
